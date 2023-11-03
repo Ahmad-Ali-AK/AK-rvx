411dc1e11a66658fbe81c3cfe80f4d99899aae10
Ahmad-Ali-AK <105027547+Ahmad-Ali-AK@users.noreply.github.com>
2023-11-03 08:54:37 +0500

    Update customize.sh

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -113,11 +113,13 @@ for s in "uninstall.sh" "service.sh"; do
 done
 
 ui_print "* Done"
-ui_print "  by MatadorProBr (github.com/MatadorProBr)"
-ui_print "  thanks to ReVanced Team (github.com/ReVanced)"
-ui_print "  thanks to inotia00 (github.com/inotia00)"
-ui_print "  thanks to j-hc (github.com/j-hc)"
-ui_print " "
+ui_print "*************************************************************************"
+ui_print "*************************************************************************"
+ui_print " By Ahmad Ali AK (https://github.com/Ahmad-Ali-AK/AK-RVX)***********"
+ui_print "**************************************************************************"
+ui_print "*************************************************************************"
+
+ui_print ""
 
 sleep 0.5
 
